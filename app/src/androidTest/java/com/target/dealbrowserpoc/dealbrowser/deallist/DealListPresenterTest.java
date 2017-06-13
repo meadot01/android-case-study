fed5fc1d85467206c630b1d8704873a117cae2ef
Todd Meadows <todd@meadowsnet.net>
2017-06-12 21:23:16 -0500

    Adding junit tests - configuration problem so not working

---
+++ app/src/androidTest/java/com/target/dealbrowserpoc/dealbrowser/deallist/DealListPresenterTest.java
@@ -0,0 +1,18 @@
+package com.target.dealbrowserpoc.dealbrowser.deallist;
+
+//TODO : Not sure what is going on here - something wrong with junit configuration
+//import static org.junit.Assert.*;
+
+
+public class DealListPresenterTest {
+//    @org.junit.Test
+//    public void start() throws Exception {
+//
+//    }
+//
+//    @org.junit.Test
+//    public void destroy() throws Exception {
+//
+//    }
+
+}
